72bc04754cf4dea67c0cfb72000e571549405d99
code <anasplrt34@gmail.com>
2023-05-25 18:53:13 +0800

    新增github actions

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import "./styles.css";
 import App from "./App.vue";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+
 // @ts-ignore
 import router from './router/index.js'
 import hls from 'highlight.js';
 import 'highlight.js/styles/atom-one-dark.css' //样式
+
 const app = createApp(App)
 
 app.directive('highlight',function (el) {
